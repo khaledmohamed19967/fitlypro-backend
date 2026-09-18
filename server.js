450522a4ee9719230bec6a122654b730c3e5408f
KhaledAboBakr0 <khaled.abobakr@homzmart.com>
2026-09-18 21:59:50 +0300

    Add logging to server.js for database connection process

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import app from './src/app.js';
 import connectDatabase from './src/config/database.js';
 
+console.log('🔥 FITLYPRO SERVER.JS LOADED');
+
+console.log('🔥 Before connectDatabase');
 await connectDatabase();
+console.log('🔥 After connectDatabase');
 
 export default app;
